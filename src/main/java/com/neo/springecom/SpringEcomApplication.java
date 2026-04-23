2370859f16845f88d013a6e07db11848b16b18ae
NeofytosIoannou <neofytosioannou4@gmail.com>
2026-04-23 19:25:27 +0300

    cache manager and config added

--- src/main/java/com/neo/springecom/SpringEcomApplication.java
+++ src/main/java/com/neo/springecom/SpringEcomApplication.java
@@ -2,7 +2,10 @@ package com.neo.springecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +13,11 @@ public class SpringEcomApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringEcomApplication.class, args);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("productImages");
     }
 
 }
